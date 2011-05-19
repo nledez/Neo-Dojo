0d2508ccf77afdb076afbd55661553c1482632a0
Nicolas Ledez <github@ledez.net>
2011-05-19 13:08:29 +0200

    Définition de grille et pas de mine

--- lib/demineur.rb
+++ lib/demineur.rb
@@ -1,2 +1,9 @@
 class Demineur
+  def size
+    4
+  end
+
+  def mine?(x, y)
+    0
+  end
 end
--- spec/demineur_spec.rb
+++ spec/demineur_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 require "demineur"
 
 describe Demineur do
-  it "dummy" do
-    dummy = 1
-    dummy.should == 1
+  it "grid" do
+    grid = Demineur.new([[0, 0, 1, 0], [0, 0, 1, 0],[0, 0, 1, 0],[0, 0, 1, 0]])
+    grid.size.should == 4
+    grid.mine?(0, 0).should == 0
   end
 end
